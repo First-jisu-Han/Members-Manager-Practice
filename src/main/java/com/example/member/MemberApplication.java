80761ad2df065550b264e807945ed4801c70ba15
Jisu Han <weblite@naver.com>
2022-01-17 15:16:00 +0900

    회원 주문 servlet 으로 구성 - HTML을 직접 자바코드안에 넣어야하는 불편함

--- src/main/java/com/example/member/MemberApplication.java
+++ src/main/java/com/example/member/MemberApplication.java
@@ -2,7 +2,9 @@ package com.example.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan   // 서블릿 작동시키려면 꼭 붙여야함
 @SpringBootApplication
 public class MemberApplication {
 
