867b1ac83748ebae8f5c8b9516ae33fdae2f5e74
Jisu Han <weblite@naver.com>
2022-01-21 23:57:24 +0900

    다른 컨트롤러이넡페이스 구현체들을 사용할 수 있게끔 하는 어뎁터 패턴 추가 - V3용 어뎁터 V4용 어뎁터

--- src/main/java/com/example/member/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/com/example/member/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -7,7 +7,11 @@ import com.example.member.web.frontcontroller.v3.controller.MemberFormController
 import com.example.member.web.frontcontroller.v3.controller.MemberListControllerV3;
 import com.example.member.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 import com.example.member.web.frontcontroller.v4.ControllerV4;
+import com.example.member.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import com.example.member.web.frontcontroller.v4.controller.MemberListControllerV4;
+import com.example.member.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import com.example.member.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import com.example.member.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -31,14 +35,20 @@ public class FrontControllerServletV5 extends HttpServlet {
         initHandlerAdapters();
     }
 
-    public void initHandlerMappingMap(){
+    public void initHandlerMappingMap() {
+        // ControllerV3 사용하기 위한 것들
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+        // ControllerV4 사용하기 위한 것들
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
+
     public void initHandlerAdapters(){
         handlerAdapters.add(new ControllerV3HandlerAdapter());
-
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
