f5a222831eec3de9b5e0d7ee59f6b5b490d2e271
Jisu Han <weblite@naver.com>
2022-01-24 18:44:06 +0900

    Spring MVC 패턴을 사용한 회원관리

---
+++ src/main/java/com/example/member/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,26 @@
+package com.example.member.web.springmvc.v1;
+
+import com.example.member.domain.Member;
+import com.example.member.domain.MemberRepository;
+import com.example.member.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+
+    }
+}
